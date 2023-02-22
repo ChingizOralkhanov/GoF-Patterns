e7cdcb40ee0a5bccb7b4543c155f28d3e8745915
Shyngyskhan Oralkhanov <soralkhanov@lsisoftware.pl>
2023-02-22 12:08:29 +0100

    Add project files.

---
+++ Strategy/Program.cs
@@ -0,0 +1,18 @@
+﻿using Strategy;
+
+//Стратегия — это поведенческий паттерн проектирования, который определяет семейство схожих алгоритмов и 
+//помещает каждый из них в собственный класс, после чего алгоритмы можно взаимозаменять прямо во время исполнения программы.
+
+var context = new Context();
+
+Console.WriteLine("Client: Strategy is set to normal sorting.");
+context.SetStrategy(new ConcreteStrategyA());
+context.DoSomeBusinessLogic();
+
+Console.WriteLine();
+
+Console.WriteLine("Client: Strategy is set to reverse sorting.");
+context.SetStrategy(new ConcreteStrategyB());
+context.DoSomeBusinessLogic();
+
+Console.Read();
